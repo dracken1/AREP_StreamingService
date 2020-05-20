f5a083f22e30fe8a45a424ba5a855162f97d7d2e
Nicolas Cardenas <nicocrash97@hotmail.com>
2020-05-20 10:56:34 -0500

    Streaming Service Front

---
+++ src/components/Player.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import Hls from 'hls.js'
+import './player.css'
+
+export default class Player extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.pausePlayHandler = this.pausePlayHandler.bind(this);
+    }
+
+    //urlprueba -> https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8
+
+    state = {
+        url: '',
+    }    
+
+    componentDidMount() {
+        const video = this.player;
+        const videoSrc = this.state.url;
+        if (Hls.isSupported() && this.player) {
+            console.log("Receiving data...")
+            const hls = new Hls();
+            hls.loadSource(videoSrc);
+            hls.attachMedia(video);
+            hls.on(Hls.Events.MANIFEST_PARSED, function () {
+                video.play();
+            });
+        }
+    }
+
+    startStreaming() {
+        const video = this.player;
+        const videoSrc = this.state.url;
+        if (Hls.isSupported() && this.player) {
+            console.log("Receiving data...")
+            console.log("url: " + this.state.url);
+            const hls = new Hls();
+            hls.loadSource(videoSrc);
+            hls.attachMedia(video);
+            hls.on(Hls.Events.MANIFEST_PARSED, function () {
+                video.play();
+            });
+        }
+    }
+
+    pausePlayHandler(){
+        if(this.player.paused){
+            this.player.play();
+        } else {
+            this.player.pause();
+        }
+    }
+
+    handleInput = (e) =>{
+        this.setState({
+            url: e.target.value,
+        })
+        console.log("Input: " + this.state.url);        
+    }
+
+    render() {
+        return (
+            <div className="playercontainer">
+                <div className="player">
+                    {this.state.url != '' && this.startStreaming()}
+                    <video className="video" onClick={this.pausePlayHandler} autoPlay={true} ref={(player) => this.player = player}></video>
+                </div>
+                <input type="text" placeholder="URL" className="urlinput" onChange={(e) => this.handleInput(e)}></input>
+            </div>
+        )
+    }
+}
